03b4d6c7a0211756a862a11c09d99178150d8f56
Ho Hoang Quan <hohoangquan939@gmail.com>
2026-08-22 04:45:07 +0700

    Luu lai file 0.py

--- 00_Helloworld/0.py
+++ 00_Helloworld/0.py
@@ -5,6 +5,6 @@ print(f"I'm passionate about programming and technology, and I enjoy learning ne
 
 def main():
     print(f"Thank you for reading my introduction!")
-
+    print(f"I really enjoy learning coding")
 
 main()
