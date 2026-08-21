2e2f4db27e2db8897b849932225867c0e1fe0648
Ho Hoang Quan <hohoangquan939@gmail.com>
2026-08-22 04:36:29 +0700

    Thay doi o file 0.py

--- 00_Helloworld/0.py
+++ 00_Helloworld/0.py
@@ -1,3 +1,4 @@
 print(f"Hello world! This is a simple Python program")
 print(f"My name is Quan")
 print(f"I'm 19 years old, and I'm currently studying at the University of Science and Technology of Da Nang")
+print(f"I'm passionate about programming and technology, and I enjoy learning new things every day!")
