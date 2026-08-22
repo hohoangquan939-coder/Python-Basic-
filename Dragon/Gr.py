92ac04b8ad33e1000f95a41427c0f793ebe6259f
Ho Hoang Quan <hohoangquan939@gmail.com>
2026-08-22 18:41:55 +0700

    Them folder moi

---
+++ Dragon/Gr.py
@@ -0,0 +1,12 @@
+def keu():
+    print("This is a function named keu in the Dragon/Gr.py file.")
+
+def VoCanh():
+    print("This is a function named VoCanh in the Dragon/Gr.py file.")
+
+def main():
+    print("This is the main function in the Dragon/Gr.py file.")
+    keu()
+    VoCanh()
+
+main()
